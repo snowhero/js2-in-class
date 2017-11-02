ff7af805e6ba5bd6376d0a81ac2a83daed9f086c
snowhero <xionghiroyuki@gmail.com>
2017-11-01 18:28:50 -0700

    Day 1 materials

---
+++ Day1/js/scripts.js
@@ -0,0 +1,22 @@
+function logMessage() {
+  console.log('Hello World!');
+};
+// logs a message when p tag is clicked
+
+var name = "Hiroyuki";
+
+// ES5 string interpolation
+var message = "Hello there," + name + "!";
+console.log(message);
+
+// ES6 string interpolation
+var secondMessage = 'Hello there, ${name}';
+// console.log(secondMessage);
+
+// Variable declaration
+var number;
+
+// Variable definition
+number = 14;
+
+console.log(number);
